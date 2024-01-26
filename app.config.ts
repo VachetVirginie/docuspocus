ba4729d84b109073fb16b1e0a844965dc2c3d1d7
virginieVachet <vachetvirginie@gmail.com>
2024-01-26 16:35:05 +0100

    improve readMe

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,7 @@
 export default defineAppConfig({
   docus: {
     title: 'POCUSDOCUS',
-    description: 'The best place to start your documentation.',
+    description: 'The best place to be.',
     image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
     socials: {
       twitter: 'nuxt_js',
