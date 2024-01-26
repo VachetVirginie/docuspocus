a2070e38387472e8d982acc3306503eedafe78bc
virginieVachet <vachetvirginie@gmail.com>
2024-01-26 16:41:13 +0100

    add style in custom btn

--- components/content/ButtonLink.vue
+++ components/content/ButtonLink.vue
@@ -17,9 +17,32 @@ defineProps({
 
 <template>
   <span class="not-prose">
-    <NuxtLink :to="href" :external="external" class="inline-flex items-center gap-1 text-white bg-gray-800 dark:bg-gray-200 dark:text-gray-950 px-3 py-1 rounded no-underline">
-      <Icon v-if="icon" :name="icon" class="w-4 h-4" />
+    <a :href="href" :class="['custom-link', external ? 'external' : '']">
+      <Icon v-if="icon" :name="icon" class="custom-icon" />
       <ContentSlot :use="$slots.default" unwrap="p" />
-    </NuxtLink>
+    </a>
   </span>
 </template>
+
+<style>
+.custom-link {
+  display: inline-flex;
+  align-items: center;
+  gap: 1rem;
+  color: white;
+  background-color: #0047e1 ; /* Remplacez par la couleur de fond souhaitée */
+  padding: 0.25rem 0.5rem;
+  border-radius: 0.25rem;
+  text-decoration: none;
+}
+
+.custom-link.external {
+  background-color: #0047e1; /* Couleur de fond différente pour les liens externes */
+}
+
+.custom-icon {
+  width: 1rem; /* Ajustez la taille de l'icône selon vos besoins */
+  height: 1rem;
+  /* Ajoutez d'autres styles d'icône si nécessaire */
+}
+</style>
